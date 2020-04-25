4226ae523d3ef16e08c339f026eae0ece14230cb
Sushma Mangalapally <sushma.m812@gmail.com>
2020-04-24 17:44:38 -0700

    Updates

---
+++ precache-manifest.05a62f5b8585b82f30f116631f98ad27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbea92bd40309536e6f66ffd0df09e87",
+    "url": "/photocollage/index.html"
+  },
+  {
+    "revision": "8fff8ef29ac1e94e5609",
+    "url": "/photocollage/static/css/main.29c3aaf3.chunk.css"
+  },
+  {
+    "revision": "820355a8c87da051184f",
+    "url": "/photocollage/static/js/2.42c73249.chunk.js"
+  },
+  {
+    "revision": "3df5ce59cb82502b4f6e2f6ed6a26954",
+    "url": "/photocollage/static/js/2.42c73249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fff8ef29ac1e94e5609",
+    "url": "/photocollage/static/js/main.ca1b72c9.chunk.js"
+  },
+  {
+    "revision": "36a04de777219c973902",
+    "url": "/photocollage/static/js/runtime-main.cc968093.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/photocollage/static/media/logo.5d5d9eef.svg"
+  }
+]);
