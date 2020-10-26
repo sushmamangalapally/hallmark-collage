c33a4d3e1638bd47d8efeec26d47aece027ea4d4
Sushma Mangalapally <sushma.m812@gmail.com>
2020-10-25 17:57:31 -0700

    Updates

---
+++ precache-manifest.9a3de79f75fc317ec76dc137e9a2ec8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a494e1f537c37beb674b5c4619a874c9",
+    "url": "/hallmark-collage/index.html"
+  },
+  {
+    "revision": "c69649f978182e8e5e2b",
+    "url": "/hallmark-collage/static/css/main.dec1201b.chunk.css"
+  },
+  {
+    "revision": "ef21378eeea4cd55fea4",
+    "url": "/hallmark-collage/static/js/2.d9b15b7b.chunk.js"
+  },
+  {
+    "revision": "c5dfa5b053142c0f9c10eb4e199d2a04",
+    "url": "/hallmark-collage/static/js/2.d9b15b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69649f978182e8e5e2b",
+    "url": "/hallmark-collage/static/js/main.efd86532.chunk.js"
+  },
+  {
+    "revision": "6c6df1031cd20171550f",
+    "url": "/hallmark-collage/static/js/runtime-main.57b3aa50.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hallmark-collage/precache-manifest.c986e4154abc8e563fa11454c4d87fb9.js"
+  "/hallmark-collage/precache-manifest.9a3de79f75fc317ec76dc137e9a2ec8d.js"
 );
 
 self.addEventListener('message', (event) => {
